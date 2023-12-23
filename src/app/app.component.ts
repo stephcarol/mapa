7db2a6f73576e50ad7f003ad470e9f4f34d64ed0
unknown <ricluper@espol.edu.ec>
2023-12-23 10:36:53 -0500

    prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,17 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { IndexComponent } from './pages/index/index.component';
 
+import { HttpClientModule } from  '@angular/common/http';
+
+//Importación del servicio
+import { ServicioProyectoService } from './providers/servicio-proyecto.service';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent,HttpClientModule],
+  providers: [ServicioProyectoService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
